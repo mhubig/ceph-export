f51f4de67d1b3a5fc34ce49b5147cc0a5043185a
Markus Hubig <mhubig@gmail.com>
2018-04-26 15:17:01 +0200

    Starts the backup file.

---
+++ backup.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+
+import delegator
+
+
+def get_list_of_images_from(pool):
+    images = delegator.chain(f'rbd list {pool} |grep ^vm')
+    return list(images.out)
+
+
+def get_snaps_of(image, pattern):
+    snaps = delegator.chain(f'rbd snap list {image} |grep {pattern}')
+    return list(snaps.out)
